b116b45f4f7167bc6e9fc8b62a188fdb6b1f80f2
Serhii-Yakimtso <serafyso@gmail.com>
2024-04-28 14:18:15 +0300

    add components

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const API_KEY = 'fb53631008a69a9fffef9c3858eef479';
+const authorization =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjUzNjMxMDA4YTY5YTlmZmZlZjljMzg1OGVlZjQ3OSIsInN1YiI6IjY2MmUyNmQ3ZjZmZDE4MDEyYjIyYzgyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WK-bOd2Lxjh_fJnvVmGNuDDB1CA1wI08YAQQD2d1NHg';
+const options = {
+  headers: {
+    Authorization: authorization,
+  },
+};
+
+export function getMovies() {}
+export function searchMovies() {}
